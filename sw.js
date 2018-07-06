4158730b9fa997ad16acc613cebdbb45bdaf3e70
jcfishmarket <40644629+jcfishmarket@users.noreply.github.com>
2018-07-06 16:00:26 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,9 @@ var filesToCache = [
   '/img/slide1.jpg',
   '/img/slide2.jpg',
   '/img/slide3.jpg',
-  '/img/header-bg.jpg'
+  '/img/header-bg.jpg',
+  'https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900',
+  'https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i'
   
 ];
 self.addEventListener('install', function(e) {
